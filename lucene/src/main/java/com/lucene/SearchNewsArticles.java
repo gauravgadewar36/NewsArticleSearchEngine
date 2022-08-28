5732f834542a3ad91607a52a99409fa2e08cc90e
gauravgadewar36 <112223035+gauravgadewar36@users.noreply.github.com>
2022-08-27 22:47:01 -0700

    Update SearchNewsArticles.java
    Uncommented code

--- lucene/src/main/java/com/lucene/SearchNewsArticles.java
+++ lucene/src/main/java/com/lucene/SearchNewsArticles.java
@@ -8,27 +8,27 @@ public class SearchNewsArticles {
 
 	public static void main(String[] args) throws IOException, ParseException {
 		
-		String jsonFilesPath = "C:\\IRProject\\data\\";
-		String indexPath = "C:\\IRProject\\Index";
-		String queryResultStorePath = "C:\\IRProject\\IndexData\\";
-		String query = "vaccine";
-		int numberOfArticlesToFetch = 5;
+// 		String jsonFilesPath = "C:\\IRProject\\data\\";
+// 		String indexPath = "C:\\IRProject\\Index";
+// 		String queryResultStorePath = "C:\\IRProject\\IndexData\\";
+// 		String query = "vaccine";
+// 		int numberOfArticlesToFetch = 5;
 		
-		LuceneIndexWriter index = new LuceneIndexWriter(jsonFilesPath, indexPath);
-		index.createIndex();
+// 		LuceneIndexWriter index = new LuceneIndexWriter(jsonFilesPath, indexPath);
+// 		index.createIndex();
 
-		LuceneIndexReader reader = new LuceneIndexReader(queryResultStorePath, indexPath);
-		reader.search(query,numberOfArticlesToFetch);
+// 		LuceneIndexReader reader = new LuceneIndexReader(queryResultStorePath, indexPath);
+// 		reader.search(query,numberOfArticlesToFetch);
 		
-//		if(args[5].equals("true")) {
-//			
-//			LuceneIndexWriter index = new LuceneIndexWriter(args[0], args[1]);
-//			index.createIndex();
-//		}
-//
-//		LuceneIndexReader reader = new LuceneIndexReader(args[2], args[1]);
-//		int numberOfTopArticles = Integer.parseInt(args[4]);
-//		reader.search(args[3],numberOfTopArticles);
+		if(args[5].equals("true")) {
+			
+			LuceneIndexWriter index = new LuceneIndexWriter(args[0], args[1]);
+			index.createIndex();
+		}
+
+		LuceneIndexReader reader = new LuceneIndexReader(args[2], args[1]);
+		int numberOfTopArticles = Integer.parseInt(args[4]);
+		reader.search(args[3],numberOfTopArticles);
 		
 	}
 
